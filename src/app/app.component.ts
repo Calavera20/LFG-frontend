1145bfbe321238fc127efe0209da7b1d10ddf36a
Calavera20 <33523344+Calavera20@users.noreply.github.com>
2022-01-18 10:53:51 +0100

    cleanup p.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 

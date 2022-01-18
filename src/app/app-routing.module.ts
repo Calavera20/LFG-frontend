0c9379a41fae27cd705cfa53fed711de479ef156
Calavera20 <33523344+Calavera20@users.noreply.github.com>
2022-01-18 10:32:30 +0100

    cleanup part 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LogInComponent } from  "./modules/main/components/log-in/log-in.component"
 import { PageNotFoundComponent } from './modules/main/components/page-not-found/page-not-found.component';
-import { SignupComponent } from './modules/main//components/signup/signup.component';
+import { SignupComponent } from './modules/main/components/signup/signup.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { FriendsManagerComponent } from './modules/friends-manager/friends-manager.component';
 import { GroupComponent } from './modules/group/group.component';
